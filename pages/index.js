2a925785c8a837924f005f4ccf9b3fe2500205b0
Tony Stewers <31263046+TonyStewers@users.noreply.github.com>
2023-10-09 14:51:23 +0000

    Adicionando scripst  e

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home(){
-    return <h1>Isabela, te amo! <br></br> Tentando quebrar uma linha sem o \n</h1>;
+function Home() {
+  return (
+    <h1>
+      Isabela, te amo! <br></br> Tentando quebrar uma linha sem o \n
+    </h1>
+  );
 }
 
 export default Home;
