a15085e4dcfdc44a8bf0972f30ac51f11433d993
Tony Stewers <31263046+TonyStewers@users.noreply.github.com>
2024-05-21 01:17:52 +0000

    Finish `/api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,23 @@
 test("Get to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  const versionDb = responseBody.dependencies.database.version;
+  expect(versionDb).toBeDefined();
+  expect(versionDb).toEqual("16.0");
+
+  const maxConnections = responseBody.dependencies.database.max_connections;
+  expect(maxConnections).toBeDefined();
+  expect(maxConnections).toEqual(100);
+
+  const currentConnections =
+    responseBody.dependencies.database.current_connections;
+  expect(currentConnections).toBeDefined();
+  expect(currentConnections).toEqual(1);
 });
